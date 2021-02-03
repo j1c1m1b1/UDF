2c493f5a50a50beca8b5ddd9d54f4bf2a770e51b
Julio Mendoza <j1c1m1b1@gmail.com>
2021-02-03 23:42:53 +0100

    Added loading for error action

--- presentation/src/main/java/com/j1c1m1b1/presentation/main/MainAction.kt
+++ presentation/src/main/java/com/j1c1m1b1/presentation/main/MainAction.kt
@@ -8,19 +8,23 @@ import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 sealed class MainAction : Action<MainState> {
+    protected val delayMillis: Long
+        get() = loadingSecondsRange.random().let { TimeUnit.SECONDS.toMillis(it) }
+
     data class SendSuccess(private val message: String) : MainAction() {
-        private val delayMillis: Long
-            get() = loadingSecondsRange.random().let { TimeUnit.SECONDS.toMillis(it) }
 
         override suspend fun perform(oldState: MainState): Flow<MainState> = flow {
             emit(MainState.Loading)
-            val previousMessage = oldState.let { it as? MainState.Complete }?.result
+            val newMessage = oldState.getNewMessage()
+            emit(MainState.Complete(result = newMessage))
+        }
+
+        /**
+         * Simulates a time consuming asynchronous successful operation
+         */
+        private suspend fun MainState.getNewMessage(): String {
             delay(delayMillis)
-            previousMessage.toNewMessage().let {
-                MainState.Complete(result = it)
-            }.also {
-                emit(it)
-            }
+            return this.let { it as? MainState.Complete }?.result.toNewMessage()
         }
 
         private fun String?.toNewMessage(): String =
@@ -31,19 +35,29 @@ sealed class MainAction : Action<MainState> {
 
         private companion object {
             const val MESSAGE_TEMPLATE = "Message: %s"
-            val loadingSecondsRange: LongRange = 1L..5L
         }
     }
 
     object SendError : MainAction() {
         override suspend fun perform(oldState: MainState): Flow<MainState> = flow {
-            IllegalArgumentException("Random exception").takeIf {
+            emit(MainState.Loading)
+            val exception = getException()
+            emit(MainState.Error(throwable = exception))
+
+        }
+
+        /**
+         * Simulates a time consuming asynchronous unsuccessful operation
+         */
+        private suspend fun getException(): Throwable? {
+            delay(delayMillis)
+            return IllegalArgumentException("Random exception").takeIf {
                 Random.nextBoolean()
-            }.let {
-                MainState.Error(throwable = it)
-            }.also {
-                emit(it)
             }
         }
+    }
+
+    private companion object {
+        val loadingSecondsRange: LongRange = 1L..5L
     }
 }
